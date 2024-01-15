fb33faf32e66dfc53931ff3aac22918cda221b14
Sergey Ivanov <Reger21a@yandex.ru>
2024-01-15 14:38:16 +0300

    Добавление автотестов

--- Tests/tasks/Test.java
+++ Tests/tasks/Test.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class EpicTest {
+class ManagerTest {
     Managers managers;
     InMemoryTaskManager testObj;
     HistoryManager testHistory;
@@ -81,9 +81,9 @@ class EpicTest {
     @Test
     public void shouldUniqueIdWhenCreateTask() {
         //При создании любой задачи id увеличивается, поэтому всегда уникален
-        testObj.createEpic("Уникальность", "Id");
+        testObj.createEpic("Уникальность", "Id"); // id 4
         assertEquals(4, testObj.getCounter());
-        testObj.createEpic("Уникальность", "Id");
+        testObj.createEpic("Уникальность", "Id"); //id 5
         assertEquals(5, testObj.getCounter());
     }
 
@@ -93,5 +93,40 @@ class EpicTest {
         assertFalse(testObj.updateSubtask(new Subtask("Такой", "не существует", 15)));
     }
 
+    //Проверка, что задача создается и ее можно найти
+    @Test
+    public void shouldCreateTaskAndFindIts() {
+        testObj.addTask("1", "2"); //id 4
+        assertNotNull(testObj.receiveOneTask(4));
+    }
+
+    //Проверка, что Subtask и Epic создаются и могут быть найдены по id
+    @Test
+    public void shouldCreateAndFindEpicAndSubtask() {
+        testObj.createEpic("1", "2"); //id 4
+        testObj.addSubTaskInEpic(4, "Подзадача", "Подзадача"); // id 5
+        assertNotNull(testObj.receiveOneEpic(4));
+        assertNotNull(testObj.receiveSubtasksUseID(5));
+    }
+
+    //Изменение статуса эпика
+    @Test
+    public void shouldChangeStatus() {
+        testObj.addSubTaskInEpic(2, "Сходить в магазин", "Купить макароны"); //id 4
+        Subtask subtask1 = new Subtask("Сходить в магазин", "Купить макароны", 4);
+        subtask1.setStatus("DONE");
+        testObj.updateSubtask(subtask1);
+        assertEquals("DONE", testObj.receiveOneEpic(2).getStatus().toString());
+    }
+
+    //Обновление статуса подзадачи
+    @Test
+    public void shouldChangeStatusOfTask() {
+        Task taskTest = new Task("Смена", "Статуса", 1);
+        taskTest.setStatus("IN_PROGRESS");
+        testObj.updateTask(taskTest);
+        assertEquals("IN_PROGRESS", testObj.receiveOneTask(1).getStatus().toString());
+    }
 
 }
+
